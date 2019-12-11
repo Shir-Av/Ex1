96be33a562bfa6b8df4dd5efb643bf579d973f31
Avraham <shirush771@gmail.com>
2019-12-12 00:14:06 +0200

    finished fg

---
+++ src/Ex1/Jsonparam.java
@@ -0,0 +1,12 @@
+package Ex1;
+
+public class Jsonparam {
+
+        public int Width;
+        public int Height;
+        public int Resolution;
+        public double[] Range_X;
+        public double[] Range_Y;
+
+
+}
